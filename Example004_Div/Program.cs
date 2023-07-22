716c84f032be07c34de3aa30409cda78546cb779
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-07-22 10:52:23 +0300

    add random to "Example 003 Sum" and "Example 004 Div"

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1, 10); //добавил случаное число от 1 до 9
+Console.WriteLine($"Первое число: {numberA}");
+double numberB = new Random().Next(1, 10); //добавил случаное число от 1 до 9
+Console.WriteLine($"Второе число: {numberB}");
+double result = numberA / numberB;
+Console.WriteLine($"Частное двух чисел: {result}");
